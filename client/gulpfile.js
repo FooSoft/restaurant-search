1ac46a15ea4336f0aed89cacc3f5ff65dc95566d
Alex Yatskov <alex@foosoft.net>
2014-09-30 17:40:31 +0900

    Cleanup

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -8,28 +8,28 @@ var uglify     = require('gulp-uglify');
 
 var paths = {
     js: [
-        'bower_components/underscore/underscore.js',
-        'bower_components/handlebars/handlebars.min.js',
-        'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/fabric/dist/fabric.min.js',
-        'bower_components/tinycolor/tinycolor.js',
-        'bower_components/bootstrap/dist/js/bootstrap.min.js',
-        'bower_components/bootstrap-select/dist/js/bootstrap-select.min.js',
-        'js/*.js'
+        './bower_components/underscore/underscore.js',
+        './bower_components/handlebars/handlebars.min.js',
+        './bower_components/jquery/dist/jquery.min.js',
+        './bower_components/fabric/dist/fabric.min.js',
+        './bower_components/tinycolor/tinycolor.js',
+        './bower_components/bootstrap/dist/js/bootstrap.min.js',
+        './bower_components/bootstrap-select/dist/js/bootstrap-select.min.js',
+        './js/*.js'
     ],
     css: [
-        'bower_components/bootstrap/dist/css/bootstrap.min.css',
-        'bower_components/bootstrap/dist/css/bootstrap-theme.min.css',
-        'bower_components/bootstrap-select/dist/css/bootstrap-select.min.css',
-        'css/*.css'
+        './bower_components/bootstrap/dist/css/bootstrap.min.css',
+        './bower_components/bootstrap/dist/css/bootstrap-theme.min.css',
+        './bower_components/bootstrap-select/dist/css/bootstrap-select.min.css',
+        './css/*.css'
     ],
     html: [
-        'index.html'
+        './html/*.html'
     ]
 };
 
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('./js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
@@ -52,7 +52,7 @@ gulp.task('pages', function() {
     var sources = gulp.src(paths.js.concat(paths.css), { read: false });
     return gulp.src(paths.html)
     .pipe(inject(sources))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['lint', 'scripts', 'styles', 'pages']);
